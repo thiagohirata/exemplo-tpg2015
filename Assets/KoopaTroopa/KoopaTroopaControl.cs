7ffcea5b141df2fff1bfb529bd4694adbc9cbcde
Thiago Hirata <thiago.hirata@gmail.com>
2015-05-13 22:07:27 -0300

    aaaa

---
+++ Assets/KoopaTroopa/KoopaTroopaControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KoopaTroopaControl : MonoBehaviour {
+	private Animator animator;
+
+	public bool flying;
+	public bool walking;
+
+	// Use this for initialization
+	void Start () {
+		this.animator = GetComponent<Animator> ();
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision) {
+		if (collision.collider.tag == "Player") {
+			animator.SetTrigger ("GotDamage");
+		}
+		if (collision.collider.tag == "Cenario") {
+			animator.SetBool ("Grounded", true);
+		}
+	}
+
+	void FixedUpdate() {
+		if (flying) {
+			this.rigidbody2D.velocity = new Vector2 (0, Mathf.Cos (Time.time) * 5);
+		} else if (walking) {
+			this.rigidbody2D.velocity = new Vector2 (-5f * this.transform.localScale.x, this.rigidbody2D.velocity.y);
+		} else {
+			this.rigidbody2D.velocity = new Vector2 (0, this.rigidbody2D.velocity.y);
+		}
+	}
+}
